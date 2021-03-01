0cdf7a9677e7e32aae02954a80a33e2d354ba039
Nicola Galeotti <niccio2802@outlook.it>
2021-03-01 19:32:00 +0100

    Add project files.

---
+++ Esercizio1DesignPattern/Riga.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio1DesignPattern
+{
+    public class Riga
+    {
+        List<Cella> celle = new List<Cella>();
+
+        internal Riga() { }
+
+        public Cella NuovaCella()
+        {
+            Cella c = new Cella();
+            celle.Add(c);
+            return c;
+        }
+    }
+}
---
+++ Esercizio1DesignPattern/Tabella.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio1DesignPattern
+{
+    public class Tabella
+    {
+        List<Riga> righe = new List<Riga>();
+
+        public Tabella() { }
+
+        public Riga NuovaRiga()
+        {
+            Riga r = new Riga();
+            righe.Add(r);
+            return r;
+        }
+    }
+}
